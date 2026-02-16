61f96bc84e64c54dfffa8e1ba29065d15886483c
aravinthsamys <aravinthsamy.2002@gmail.com>
2026-02-16 13:53:52 +0530

    sd

--- controller/settings.py
+++ controller/settings.py
@@ -152,14 +152,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'teramediainfo@gmail.com'
-EMAIL_HOST_PASSWORD = 'hgqnkearalskvkah'
-DEFAULT_FROM_EMAIL = 'teramediainfo@gmail.com'
+import os
+EMAIL_BACKEND = os.getenv("EMAIL_BACKEND","django.core.mail.backends.console.EmailBackend")
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
+EMAIL_HOST_USER =  os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD =  os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL =  EMAIL_HOST_USER
 
 DEPARTMENT_EMAIL = 'algetss223@gmail.com'
 
